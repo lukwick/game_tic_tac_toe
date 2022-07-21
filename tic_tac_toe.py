bddc6b59382eb604f2d9288b7603f6aae2298010
lukwick <wick.programming@mailbox.org>
2022-07-21 17:43:20 +0200

    removed redundant comments

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -17,7 +17,6 @@ import time     # To allow to have pauses after printed text and computer's choi
 # FUNCTIONS
 # ------------------------------------------------------------
 
-# Explain playing field
 def explain_playing_field():
     print(f"""
     1 | 2 | 3
@@ -27,7 +26,7 @@ def explain_playing_field():
     7 | 8 | 9
     """)
 
-# Show playing field
+
 def show_playing_field():
     print_waiting_text("The field looks now like this:")
     
@@ -40,24 +39,23 @@ def show_playing_field():
     """,
     3) # 3 Seconds waiting time, otherwise it is too quick
 
-# Print text with one free line before and after
-def print_text(text, before=1, after=1):
-    for i in range(before):
-        print("\n")
-    print(text)
-    for i in range(after):
-        print("\n")
 
-# Show waiting text "..."
-def print_waiting_text(txt = "...", seconds = 2):
+def print_text(text):
+    print("\n")
+    print(text)
+    print("\n")
+
+
+def print_waiting_text(txt, seconds = 2):
     print("\n")
     print(txt)
-    time.sleep(seconds)
     print("\n")
+    time.sleep(seconds)
 
-# Press enter function
-def press_enter(text = "Press Enter to continue\n"):
-    input(text)
+
+def press_enter():
+    input("Press Enter to continue\n")
+
 
 # Ensure to get an integer as input
 def get_int():
@@ -76,16 +74,16 @@ def get_int():
         choice = int(choice)
         return choice
     
-    # If not, redo the input
+    # If not possible, redo the input
     except ValueError:
         print_waiting_text("Wrong input, you have to choose a number from 1 to 9. Or you can press 'q' to leave the game.")
         return get_int()
 
-# Remove done choices
-def remove_option(field_spot):
-    player_options.remove(field_spot)
 
-# Player makes choice
+def remove_option(field_spot):
+    field_options.remove(field_spot)
+
+
 def player_makes_choice():
 
     while True:
@@ -93,18 +91,17 @@ def player_makes_choice():
         player_choice = get_int()
 
         # If choice is not available, help the player which options are left
-        if player_choice not in player_options:
+        if player_choice not in field_options:
             print_waiting_text("Wrong input, you have to choose a number from 1 to 9, which is still free.")
             print_waiting_text("The following options are still available:")
-            for x in player_options:
+            for x in field_options:
                 print(x)
             continue
         
         # Choice is valid
         else:
             break
-            
-    # Player choice is printed   
+             
     print_waiting_text(f"You have choosen the {field_names[player_choice]}.")
     
     # Playing field and display field is updated
@@ -114,19 +111,18 @@ def player_makes_choice():
     # Choice is removed from the left choices
     remove_option(player_choice)
 
-    # Show the new playing field
     show_playing_field()
 
-# Computer makes choice
+
 def computer_makes_choice():
 
-    # Print "processing time"
+    # Print "fake processing time"
     print_waiting_text("The computer is preparing its next move!")
     print_waiting_text("The computer is thinking ...")
-    print_waiting_text(seconds = 1)
+    print_waiting_text("...")
 
     # Computer makes a random choice from the left over options
-    computer_choice = int(random.choice(player_options)-1)
+    computer_choice = int(random.choice(field_options)-1)
     print_waiting_text(f"The computer has choosen the {field_names[computer_choice+1]}.")
 
     # Update the playing field and display field
@@ -136,13 +132,11 @@ def computer_makes_choice():
     # Remove the option from the option list
     remove_option(computer_choice+1)
 
-    # Show the new playing field
     show_playing_field()
 
-# Check the winning conditions. It is adjustable for each player.
+
 def check_if_won(player):
     f = field_invisible
-
     # Rows / columns / diagonals
     if f[0] + f[1] + f[2] == 3 * player \
         or f[3] + f[4] + f[5] == 3 * player \
@@ -161,10 +155,12 @@ def check_if_won(player):
 # ------------------------------------------------------------
 
 field_invisible = [0 for i in range(9)]     # Field to save the current game situation
- 
-field_display = ["." for i in range(9)]     # Field to present the current game in a more presentable matter
 
-field_names = {        # Giving more descriptive names for the field names
+field_options = [i for i in range(1, 10)]   # Field options left to choose from
+ 
+field_display = [" " for i in range(9)]     # Field to show the current game in a more presentable matter
+
+field_names = {                             # Give more descriptive names for the field names
     1: "top left (position 1)",
     2: "top middle (position 2)",
     3: "top right (position 3)",
@@ -175,8 +171,6 @@ field_names = {        # Giving more descriptive names for the field names
     8: "bottom middle (position 8)",
     9: "bottom right (position 9)"
      }
-
-player_options = [i for i in range(1, 10)]  # Options left to choose from
 
 
 
@@ -212,28 +206,25 @@ print_text("Alright? Let's go! :)")
 press_enter()
 
 
+
 ## THE ACTUAL GAME CODE
 ## --------------------
 
 while True:
 
-    # Player makes choice
     player_makes_choice()    
 
-    # Check if player has won
     if check_if_won(1) == 1:
         print_waiting_text("You won the game! :-)")
         break
 
     # Check if field has space left
-    if len(player_options) == 0:
+    if len(field_options) == 0:
         print_waiting_text("No space left, it's a draw!")
         break
 
-    # Computer makes choice
     computer_makes_choice()
 
-    # Check if computer has won
     if check_if_won(-1) == -1:
         print_waiting_text("The computer won the game! :-(")
         break
